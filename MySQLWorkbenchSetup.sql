2384b167fb88687281b32435812379b34a8ae667
Your Name <you@example.com>
2021-06-07 17:24:27 -0400

    ...

--- MySQLWorkbenchSetup.sql
+++ MySQLWorkbenchSetup.sql
@@ -1,10 +1,11 @@
 use lead_gen_business;
-
 insert into clients(first_name, last_name, email, joined_datetime) 
 values ("Jose","Sanchez","jose@inf.cl",now());
 
+use lead_gen_business;
 select * from clients;
 
+use lead_gen_business;
 update clients set 
 first_name = "Ignacio",
 last_name = "Ulloa", 
@@ -12,4 +13,5 @@ email = "ignacio@inf.cl",
 joined_datetime = now()
 where id = 11;
 
+use lead_gen_business;
 delete from clients where id = 11;
